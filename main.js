a8db01d44aa9a3a660f9f054c7c8e61ab690d6fb
Victor Onyebuchi <victorchike247@gmail.com>
2021-05-21 21:44:57 +0100

    used dataset

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 const endpoint = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 
-    d3.select("#barchart").selectAll("div")
-      .data(dataset)
-      .enter()
-      .append("div")
-      .attr("class", "bar")
-      .style("height", (d) => (d + "px")) 
-
   
 fetch(endpoint)
 .then(response => response.json())
 .then(data => {
   console.log(data.data[0]);
+  const dset = data.data;
+  d3.select("#barchart").selectAll("div")
+      .data(dset)
+      .enter()
+      .append("div")
+      .attr("class", "bar")
+      .style("height", (d) => (d[1] / 100 + "px")) 
 })
 
 
