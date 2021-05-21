352f680804cd5a32ee62bf1d183190eb0ef13842
Victor Onyebuchi <victorchike247@gmail.com>
2021-05-21 19:34:28 +0100

    added the endpoint

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+const endpoint = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 
     d3.select("#barchart").selectAll("div")
       .data(dataset)
@@ -6,4 +7,13 @@ const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
       .append("div")
       .attr("class", "bar")
       .style("height", (d) => (d + "px")) 
+
+  
+fetch(endpoint)
+.then(response => response.json())
+.then(data => {
+  console.log(data.data[0]);
+})
+
+
       
