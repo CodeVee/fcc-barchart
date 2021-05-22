41f2204a5e6b20132dcf484afb79caa3e991039f
Victor Onyebuchi <victorchike247@gmail.com>
2021-05-22 09:19:20 +0100

    added tooltip

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 const endpoint = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
-const width = 500;
-const height = 100;
+const width = 800;
+const height = 500;
   
 fetch(endpoint)
 .then(response => response.json())
@@ -21,25 +21,29 @@ fetch(endpoint)
       .attr("height", height);
 
   svg.selectAll("rect")
-      .data(dataset)
+      .data(dset)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * 30)
-      .attr("y", (d, i) => height - 3 * d)
-      .attr("width", 25)
-      .attr("height", (d, i) => d * 3)
+      .attr("x", (d, i) => i * 3)
+      .attr("y", d => height - d[1]/50)
+      .attr("width", 1.5)
+      .attr("height", d => d[1]/50)
       .attr("fill", "navy")
-      .attr("class", "ba")
+      .attr("class", "bar")
+      .attr("data-date", d => d[0])
+      .attr("data-gdp", d => d[1])
       .append("title")
-      .text(d => d)
+      .attr("id", "tooltip")
+      .attr("data-date", d => d[0])
+      .text(d => d[0])
 
   svg.selectAll("text")
       .data(dataset)
       .enter()
       .append("text")
-      .text(d => d)
-      .attr("x", (d, i) => i * 30)
-      .attr("y", d => height - (d * 3 + 3))
+      .text(d => d[0])
+      .attr("x", (d, i) => i * 3)
+      .attr("y", d => height - (d[1]/50) -3)
 
 })
 
